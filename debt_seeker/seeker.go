86ccacf34a6787d127a490a186d4cf0b6332cf2c
Konstantin Zhikharev <hablof@yandex.ru>
2023-05-24 15:40:58 +0300

    bug fixed

--- debt_seeker/seeker.go
+++ debt_seeker/seeker.go
@@ -13,7 +13,7 @@ import (
 
 const (
 	usTreasuryEndpoint = "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/debt_to_penny"
-	rawParams          = "fields=tot_pub_debt_out_amt,record_date&filter=record_date:lte:2023-05-19&sort=-record_date&page[size]=1"
+	rawParams          = "fields=tot_pub_debt_out_amt,record_date&sort=-record_date&page[size]=1"
 )
 
 type dataRecord struct {
